f613ae705faf218254804044fd616452d74ed7e4
Baradosso <105291659+Baradosso@users.noreply.github.com>
2025-12-10 13:50:01 +0100

    Update dbh.inc.php

--- Functions/includes/dbh.inc.php
+++ Functions/includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
-$serverName = "mariadb106.server021046.nazwa.pl";
-$dBUsername = "server021046_Smile";
-$dBPassword = "Adaschuj321";
-$dBName = "server021046_Smile";
+$serverName = "";
+$dBUsername = "";
+$dBPassword = "";
+$dBName = "";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
